62588f70ad0256785ea994dbccf5f047d3ea5b61
Ahmed7bakr <a.bakr2472001@gmail.com>
2023-11-20 16:26:24 +0200

    finished restframe Api Django

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
-from .views import article_list,article_detail,ArticleAPIView,ArticleDetail,GenericAPIView
+from django.urls import path,include
+from .views import article_list,article_detail,ArticleAPIView,ArticleDetail,GenericAPIView,ArticleViewSet
+from rest_framework.routers import DefaultRouter
 
+
+
+router = DefaultRouter()
+router.register('article',ArticleViewSet,basename='article')
 
 urlpatterns = [
+
+    path('viewset/', include(router.urls)),
+    path('viewset/<int:pk>/', include(router.urls)),
     path('article/',article_list),
     path('articles/', ArticleAPIView.as_view()),
     path('generics/article/<int:id>/', GenericAPIView.as_view()),
